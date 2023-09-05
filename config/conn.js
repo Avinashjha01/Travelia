dd06a36c6e69c480fa15e1e08f2dc20e503f44fa
Avinash Jha <114860966+Avinashjha01@users.noreply.github.com>
2023-09-05 18:46:26 +0530

    Update conn.js

--- config/conn.js
+++ config/conn.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-//const DB= process.env.DATABASE;
+const DB= process.env.DATABASE;
 
 
-mongoose.connect("mongodb+srv://Avinash:1234@cluster0.4oakmgt.mongodb.net/Codesoft?retryWrites=true&w=majority")
+mongoose.connect(DB)
 .then(()=>{
     console.log("Mongodb connected Successfully");
 })
